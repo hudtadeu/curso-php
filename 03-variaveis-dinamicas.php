e3bf9d1e329e1dd0c7309bbe582d5fb20096a97d
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-05 16:03:54 -0300

    Initial commit

--- 03-variaveis-dinamicas.php
+++ 03-variaveis-dinamicas.php
@@ -1,2 +1,15 @@
 <?php
 //Variáveis variáveis
+$bebida = "refrigerante";
+
+$$bebida = "Guaraná";
+
+echo $refrigerante;
+echo "<br>";
+
+/////////////////////////
+
+$destino = "cidade";
+$$destino = "Rio Piracicaba";
+
+echo $cidade;
