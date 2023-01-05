af83de8c7586dc37e5ac0f107b15c2a72bd8ac89
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-05 12:18:29 -0300

    Initial commit

---
+++ 01-fundamentos_basicos.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Fundamentos básicos</title>
+</head>
+
+<body>
+  <?php
+  echo "Meu nome é Hudson <br>";
+  print "Minha idade é 24";
+  ?>
+</body>
+
+</html>
