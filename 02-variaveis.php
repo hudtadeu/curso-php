018058ab842a96d24dcaca26156c12b6e00123c9
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-05 15:54:35 -0300

    aula de variáveis

--- 02-variaveis.php
+++ 02-variaveis.php
@@ -1,2 +1,36 @@
 <?php
 //Meus dados
+$nome = "Hudson Tadeu";
+$idade = 24;
+$altura = 1.75;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+// Alterando meus dados
+$nome = "Gilson Costa";
+$idade = 58;
+$altura = 1.85;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+echo "<hr>";
+
+$carro = "Hilux";
+$meunome = "Hudson";
+
+echo $carro;
+echo "<br>";
+echo $meunome;
+
+
+// Nomes válidos
+//$nome
+//$_meunome
+//$carro10
+//$nota1
+//$meu_nome
+//$meuNome
+
+// Nomes inválidos
+//$12312nome não pode inciar com números 
+//$carro@*% não pode usar caracteres especiais
+//$meu carro não pode espaço
