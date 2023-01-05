4449e7a1f54d2f8fc635499a33ea5d182491854a
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-05 18:17:24 -0300

    Escopos de variáveis

---
+++ 06-escopo-de-variaveis.php
@@ -0,0 +1,33 @@
+<?php
+
+// ESCOPO GLOBAL
+$nome = "Hudson Tadeu";
+$a = 1;
+$b = 3;
+$c = 7;
+
+function exibeNome() {
+  global $nome;
+  echo $nome;
+}
+
+exibeNome();
+echo "<hr>";
+////////////////////////
+
+function exibeCidade() {
+  // ESCOPO LOCAL
+  global $cidade;
+  $cidade = "Rio de Janeiro";
+}
+
+exibeCidade();
+echo $cidade;
+echo "<hr>";
+//////////////////////////
+
+function soma() {
+  echo $GLOBALS['a'] + $GLOBALS['b'] + $GLOBALS['c'];
+}
+
+soma();
