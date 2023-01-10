655a73e41fc0a5747dbe6e81473f6e716ac0b12c
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-09 21:12:29 -0300

    While e do while

---
+++ 17-while-dowhile.php
@@ -0,0 +1,19 @@
+<?php
+// while & do while
+
+$contador = 1;
+
+while ($contador <= 10) :
+  echo "Contador é $contador <br>";
+  $contador++;
+
+endwhile;
+
+echo "<hr>";
+
+$contador = 1;
+
+do {
+  echo "Contador é $contador <br>";
+  $contador++;
+} while ($contador <= 10);
