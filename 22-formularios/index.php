c8ac6f3c7dd15af4562de923a7b33bb39e05cbdd
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-12 21:00:45 -0300

    Sanitização

--- 22-formularios/index.php
+++ 22-formularios/index.php
@@ -9,6 +9,12 @@
   FILTER_VALIDATE_FLOAT
   FILTER_VALIDATE_IP
   FILTER_VALIDATE_URL
+  ///////Sanitização////// limpar variaveis
+  Funções (filter_input - filter_var)
+  FILTER_SANITIZE_SPECIAL_CHARS
+  FILTER_SANITIZE_INT
+  FILTER_SANITIZE_EMAIL
+  FILTER_SANITIZE_URL
 */
   ?>
 
@@ -18,7 +24,7 @@
     $erros = array();
 
     // Validações
-    if (!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)) :
+    /*    if (!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)) :
       $erros[] = "Idade precisa ser um inteiro";
     endif;
 
@@ -36,6 +42,25 @@
 
     if (!$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL)) :
       $erros[] = "URL inválido";
+    endif; */
+    // Sanitização
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+    $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
+    if (filter_var($idade, FILTER_VALIDATE_INT)) :
+      $ERROS[] = "Idade precisa ser um inteiro";
+    endif;
+
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) :
+      $ERROS[] = "Email inválido";
+    endif;
+
+
+    $url = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_URL);
+    if (filter_var($email, FILTER_VALIDATE_URL)) :
+      $ERROS[] = "URL inválida";
     endif;
 
     // exibindo mensagens
@@ -50,10 +75,9 @@
   ?>
 
   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    Nome: <input type="text" name="peso"><br>
     Idade: <input type="text" name="idade"><br>
     Email: <input type="text" name="email"><br>
-    Peso: <input type="text" name="peso"><br>
-    IP: <input type="text" name="ip"><br>
     URL: <input type="text" name="url"><br>
     <button type="submit" name="enviar-formulario"> Enviar </button><br>
   </form>
