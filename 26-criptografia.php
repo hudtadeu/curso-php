8ec1fce36b19cd2744ac6df9b53ea014f44a953f
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-26 15:02:19 -0300

    Senhas seguras com password hash

--- 26-criptografia.php
+++ 26-criptografia.php
@@ -8,5 +8,20 @@ echo "Sua senha é:" . base64_decode($novasenha); //função para decodificar
 echo "<hr>";
 
 // so consegue cripitografar - md5/sha1
-echo "Md5: " . md5($senha). "<br>";
+echo "Md5: " . md5($senha) . "<br>";
 echo "Sha1: " . sha1($senha);
+
+echo "<hr>";
+
+// senhas seguras com password hash
+$senha = "123456";
+$senha_db = '$2y$10$LHjuWTqOMsnaA0Q9L2SIce19az.36aUG2W5gpre/2dmXyia1Z0fNK';
+
+/* $senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
+echo $senhaSegura. "<br>"; */
+
+if (password_verify($senha, $senha_db)) :
+  echo "Senha válida";
+else :
+  echo "Senha inválida";
+endif;
