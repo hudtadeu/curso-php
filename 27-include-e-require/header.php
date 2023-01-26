42b769ca18c1cde9e4377d283a633841f1de1961
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-26 15:50:23 -0300

    Include / Include_Once / Require / Require_Once

---
+++ 27-include-e-require/header.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+  <title>Include e Require</title>
+  <meta charset="UTF-8">
+</head>
+<body>
---
+++ 27-include-e-require/index.php
@@ -0,0 +1,23 @@
+<?php
+//include retorna aviso
+include 'header.php';
+?>
+
+<?php echo "Olá pessoal"; ?>
+
+<?php
+include 'footer.php'; 
+?>
+
+<?php
+//require interrompe o script
+require 'header.php';
+?>
+
+<?php echo "Olá pessoal"; ?>
+
+<?php
+require 'footer.php'; 
+//com  _once saber se ja foi incluido ou não
+?>
+
