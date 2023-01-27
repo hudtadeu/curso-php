73ba3007ef6ef2085dea455b3333f87a388759c4
Hudson Tadeu <hudinvesting@gmail.com>
2023-01-27 14:41:50 -0300

    Adicionar novo cliente

--- 28-crud/index.php
+++ 28-crud/index.php
@@ -1,18 +1,38 @@
-<!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<?php
+//Header
+include_once 'includes/header.php';
+?>
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+<div class="row">
+  <div class="col s12 m6 push-m3">
+    <h3 class="light"> Clientes </h3>
+    <table class="striped">
+      <thead>
+        <tr>
+          <th>Nome:</th>
+          <th>Sobrenome:</th>
+          <th>Email:</th>
+          <th>Idade:</th>
+        </tr>
+      </thead>
 
-    <body>
+      <tbody>
+        <tr>
+          <td>Hudson</td>
+          <td>Tadeu</td>
+          <td>hudsontadeu2010@hotmal.com</td>
+          <td>23</td>
+          <td><a href="" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+          <td><a href="" class="btn-floating red"><i class="material-icons">delete</i></a></td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    <a href="" class="btn">Adicionar Clientes</a>
+  </div>
+</div>
 
-      <!--JavaScript at end of body for optimized loading-->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    </body>
-  </html>
+<?php
+//Footer
+include_once 'includes/footer.php';
+?>
---
+++ 28-crud/php_action/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+// Conexão com banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "crud";
+
+$connect = mysqli_connect()
